f37f19e7f0d2b4e88af43766cd017637e3dc9109
Talaat Magdy <talaatmagdy75@gmail.com>
2021-08-24 00:47:48 +0200

    support login and logout for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    has_secure_password
+  has_secure_password
 
-    validates :email, presence: true, format: {with: /\A[^@\s]+@[^@\s]+\z'/i, message: "email must be valid" }
+  validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/i, message: 'email must be valid' }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   get 'about', to: 'about#index'
+
   get 'sign_up', to: 'registrations#new'
   post 'sign_up', to: 'registrations#create'
+
+  delete 'logout', to: 'sessions#destroy'
+
+  get 'sign_in', to: 'sessions#new'
+  post 'sign_in', to: 'sessions#create'
 
   root 'main#index'
 end
