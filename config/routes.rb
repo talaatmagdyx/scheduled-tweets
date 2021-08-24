ef04bbf683fd08da921280e1e140d5362e48bad3
Talaat Magdy <talaatmagdy75@gmail.com>
2021-08-24 11:41:33 +0200

    support reset password

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,11 @@ Rails.application.routes.draw do
   get 'sign_in', to: 'sessions#new'
   post 'sign_in', to: 'sessions#create'
 
+  get 'password/reset', to: 'password_resets#new'
+  post 'password/reset', to: 'password_resets#create'
+
+  get 'password/reset/edit', to: 'password_resets#edit'
+  patch 'password/reset/edit', to: 'password_resets#update'
+
   root 'main#index'
 end
