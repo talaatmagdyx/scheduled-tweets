85edfae120138586c1ca4742eb4856ecb8170a3f
Talaat Magdy <talaatmagdy75@gmail.com>
2021-08-24 04:17:30 +0200

    support update password

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/i, message: 'email must be valid' }
+  validates :password, :password_confirmation, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   get 'sign_up', to: 'registrations#new'
   post 'sign_up', to: 'registrations#create'
 
+  get 'password', to: 'passwords#edit', as: :edit_password
+  patch 'password', to: 'passwords#update'
+
   delete 'logout', to: 'sessions#destroy'
 
   get 'sign_in', to: 'sessions#new'
