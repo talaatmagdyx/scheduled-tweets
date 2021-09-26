ac652395b4d1721e6425dd10d24edbc6b34c51e6
Talaat Magdy <talaatmagdy75@gmail.com>
2021-09-26 05:29:26 +0200

    publish to twitter

--- app/models/twitter_account.rb
+++ app/models/twitter_account.rb
@@ -2,4 +2,13 @@ class TwitterAccount < ApplicationRecord
 
   has_many :tweets
   belongs_to :user
+
+  def client
+    Twitter::REST::Client.new do |config|
+      config.consumer_key        = Rails.application.credentials.dig(:twitter, :api_key)
+      config.consumer_secret     = Rails.application.credentials.dig(:twitter, :api_secret)
+      config.access_token        = token
+      config.access_token_secret = secret
+    end
+  end
 end
